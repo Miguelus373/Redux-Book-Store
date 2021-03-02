d74c84a7001f8691fa74570752008c715e7198ff
Laguna1 <lagunazoomarket@gmail.com>
2021-03-02 22:08:31 +0200

    Make the BooksForm component stateful

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { CREATE_BOOK } from '../actions/index';
 
-const category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const categories = ['Category', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForms = () => (
-  <>
-    <input />
-    <select>
-      {category.map(cat => (<option key={cat}>{cat}</option>))}
-    </select>
-    <button type="submit">
-      Add Book
-    </button>
-  </>
-);
+class BooksForms extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-export default BooksForms;
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    const { title, category } = this.state;
+    return (
+      <div className="books-form">
+        <input onChange={this.handleChange} value={title} name="title" type="text" placeholder="Book title" />
+        <select onChange={this.handleChange} value={category} name="category">
+          {categories.map(category => (<option key={category}>{category}</option>))}
+        </select>
+        <button type="submit">
+          Add Book
+        </button>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addBook: book => {
+    dispatch(CREATE_BOOK(book));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(BooksForms);
